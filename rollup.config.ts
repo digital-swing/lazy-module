1a927f931f30eac56c68dfcec6fcc274f086dd8a
Lucas Demea <lucas@digital-swing.com>
2022-08-16 20:20:22 +0200

    chore: initial Commit

---
+++ rollup.config.ts
@@ -0,0 +1,87 @@
+// import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+// import renameNodeModules from 'rollup-plugin-rename-node-modules';
+import resolve from '@rollup/plugin-node-resolve';
+import summary from 'rollup-plugin-summary';
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [esbuild()],
+    output: {
+      file: `dist/cjs/index.js`,
+      format: 'cjs',
+      sourcemap: true,
+      // exports: 'default',
+    },
+    external: ['intersection-observer', 'requestidlecallback-polyfill'],
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [esbuild(), resolve(), nodePolyfills(), summary()],
+    output: {
+      file: `dist/esm/index.js`,
+      format: 'esm',
+      sourcemap: true,
+    },
+    external: ['intersection-observer', 'requestidlecallback-polyfill'],
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [dts()],
+    output: {
+      file: `types/index.d.ts`,
+      format: 'es',
+    },
+  },
+  // {
+  //   input: 'src/index.ts',
+  //   output: [
+  //     {
+  //       // exports: 'named',
+  //       sourcemap: true,
+  //       dir: 'dist/esm',
+  //       // file: 'dist/esm/index.js',
+  //       format: 'esm',
+  //       preserveModules: true,
+  //       preserveModulesRoot: 'src',
+  //     },
+  //     {
+  //       // exports: 'named',
+  //       sourcemap: true,
+  //       file: 'dist/cjs/index.js',
+  //       format: 'cjs',
+  //     },
+  //   ],
+  // plugins: [
+  //   resolve(),
+  //   nodePolyfills(),
+  //   commonjs({
+  //     include: /node_modules/,
+  //   }),
+  //   summary(),
+  //   renameNodeModules('external'),
+  //   dts(),
+  // ],
+  // external: ['object-hash'],
+  // },
+  // plugins: [
+  //   resolve(),
+  //   nodePolyfills(),
+  //   commonjs({
+  //     include: /node_modules/,
+  //   }),
+  //   summary(),
+  //   renameNodeModules('external'),
+  //   dts(),
+  // ],
+  // external: ['object-hash'],
+  // },
+  // {
+  //   // path to your declaration files root
+  //   input: './dist/dts/index.d.ts',
+  //   output: [{ file: 'types/index.d.ts', format: 'es' }],
+  //   plugins: [dts()],
+  // },
+];
---
+++ src/index.ts
@@ -0,0 +1,120 @@
+import 'intersection-observer';
+import 'requestidlecallback-polyfill';
+
+type LazyModuleConfig = {
+  /**
+   * Element to watch
+   */
+  trigger: string;
+  /**
+   * Module to load
+   *
+   * @return  {<unknown>}The module that has been loaded. It can be anything: a class, a constant...
+   */
+  loader: () => Promise<unknown>;
+  /**
+   * What should be executed after the module has been imported.
+   *
+   * @param   {unknown}      module   The imported module
+   * @param   {HTMLElement}  element  The element that triggered the module import
+   *
+   * @return  {<module>}              [return description]
+   */
+  callback: (module: unknown, element?: HTMLElement) => unknown;
+
+  /**
+   * Should the module be loaded when the trigger enters the viewport, or deferred to when the browser is idle.
+   */
+  lazy: boolean;
+
+  /**
+   * LazyModules dependencies, in case one or many other modules must be loaded first.
+   */
+  dependsOn: LazyModule | LazyModule[];
+
+  /**
+   * Intersection Observer options
+   */
+  observerOptions: {
+    /**
+     * The element used as viewport
+     */
+    root: Element | Document | null;
+
+    /**
+     * The distance from the root where the trigger will load the module
+     */
+    rootMargin: string;
+
+    /**
+     * Percentage of trigger visibility inside root
+     */
+    thresholds: Array<number>;
+  };
+};
+
+export class LazyModule {
+  trigger = 'root';
+  loader: () => Promise<unknown> = () => Promise.resolve();
+  callback: (module: unknown, element?: Element) => unknown = () => {};
+  lazy = true;
+  dependsOn: LazyModule[] = [];
+  observerOptions = {
+    root: document,
+    rootMargin: '0px 0px 0px 0px',
+    thresholds: 0.0,
+  };
+  #targets: NodeListOf<Element>;
+  constructor(config: Partial<LazyModuleConfig>) {
+    Object.assign(this, config);
+    this.#targets = document.querySelectorAll(this.trigger);
+  }
+
+  init = () => {
+    //if component found on page
+    if (this.#targets) {
+      if ('requestIdleCallback' in window && !this.lazy) {
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        window.requestIdleCallback(this._loadModule);
+      } else {
+        const observer = new IntersectionObserver(
+          this._handleIntersect,
+          this.observerOptions
+        );
+        this.#targets.forEach((el) => {
+          observer.observe(el);
+        });
+      }
+    }
+  };
+
+  _loadModule = async () => {
+    // wait for all dependencies to be loaded
+    await Promise.all(
+      ([] as LazyModule[])
+        .concat(this.dependsOn)
+        .map((module) => module._loadModule())
+    ).then(() => {
+      this.loader()
+        .then((module) => {
+          this.#targets.forEach((element) => {
+            this.callback(module, element);
+          });
+        })
+        .catch((error: Error) => {
+          console.error(error.message);
+        });
+    });
+  };
+
+  _handleIntersect = (
+    domElements: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) => {
+    domElements.forEach((el) => {
+      if (el.isIntersecting) {
+        this._loadModule().finally(() => observer.disconnect());
+      }
+    });
+  };
+}
