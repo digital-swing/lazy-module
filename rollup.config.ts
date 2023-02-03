a0573b5383feffe5519fdecfa03ac5ad04dc30e7
Lucas Demea <lucas@digital-swing.com>
2023-02-03 19:02:07 +0100

    build: update artifact config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,31 +1,52 @@
-// import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
-import esbuild from 'rollup-plugin-esbuild';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
-// import renameNodeModules from 'rollup-plugin-rename-node-modules';
-import resolve from '@rollup/plugin-node-resolve';
-import summary from 'rollup-plugin-summary';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+// import globImport, { camelCase } from 'rollup-plugin-glob-import';
+// import globImport from 'rollup-plugin-glob-import';
+import pkg from './package.json' assert { type: 'json' };
+import renameNodeModules from 'rollup-plugin-rename-node-modules';
+// import summary from 'rollup-plugin-summary';
+// import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
 export default [
   {
     input: 'src/index.ts',
-    plugins: [esbuild()],
-    output: {
-      file: `dist/cjs/index.js`,
-      format: 'cjs',
-      sourcemap: true,
-      // exports: 'default',
-    },
     external: ['intersection-observer', 'requestidlecallback-polyfill'],
-  },
-  {
-    input: 'src/index.ts',
-    plugins: [esbuild(), resolve(), nodePolyfills(), summary()],
-    output: {
-      file: `dist/esm/index.js`,
-      format: 'esm',
-      sourcemap: true,
-    },
-    external: ['intersection-observer', 'requestidlecallback-polyfill'],
+    output: [
+      {
+        dir: 'dist',
+        entryFileNames: '[name].cjs',
+        exports: 'named',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        dir: 'dist',
+        entryFileNames: '[name].mjs',
+        exports: 'named',
+        format: 'es',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        sourcemap: true,
+      },
+      {
+        exports: 'named',
+        file: pkg.browser,
+        format: 'umd',
+        name: 'lazyModule',
+        // plugins: [terser()],
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs({
+        include: /node_modules/,
+      }),
+      typescript({ sourceMap: true }),
+      renameNodeModules('external'),
+    ],
   },
   {
     input: 'src/index.ts',
@@ -35,53 +56,4 @@ export default [
       format: 'es',
     },
   },
-  // {
-  //   input: 'src/index.ts',
-  //   output: [
-  //     {
-  //       // exports: 'named',
-  //       sourcemap: true,
-  //       dir: 'dist/esm',
-  //       // file: 'dist/esm/index.js',
-  //       format: 'esm',
-  //       preserveModules: true,
-  //       preserveModulesRoot: 'src',
-  //     },
-  //     {
-  //       // exports: 'named',
-  //       sourcemap: true,
-  //       file: 'dist/cjs/index.js',
-  //       format: 'cjs',
-  //     },
-  //   ],
-  // plugins: [
-  //   resolve(),
-  //   nodePolyfills(),
-  //   commonjs({
-  //     include: /node_modules/,
-  //   }),
-  //   summary(),
-  //   renameNodeModules('external'),
-  //   dts(),
-  // ],
-  // external: ['object-hash'],
-  // },
-  // plugins: [
-  //   resolve(),
-  //   nodePolyfills(),
-  //   commonjs({
-  //     include: /node_modules/,
-  //   }),
-  //   summary(),
-  //   renameNodeModules('external'),
-  //   dts(),
-  // ],
-  // external: ['object-hash'],
-  // },
-  // {
-  //   // path to your declaration files root
-  //   input: './dist/dts/index.d.ts',
-  //   output: [{ file: 'types/index.d.ts', format: 'es' }],
-  //   plugins: [dts()],
-  // },
 ];
