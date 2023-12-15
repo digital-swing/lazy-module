e38beabd9b72332b440d038f185481b2dbc73730
Lucas Demea <lucas@digital-swing.com>
2023-12-15 11:50:14 +0100

    build: setup build config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,13 @@
-import dts from 'rollup-plugin-dts';
+// import dts from 'rollup-plugin-dts';
 import commonjs from '@rollup/plugin-commonjs';
-import esbuild from 'rollup-plugin-esbuild';
+// import esbuild from 'rollup-plugin-esbuild';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import pkg from './package.json' assert { type: 'json' };
+import terser from '@rollup/plugin-terser';
+// import pkg from './package.json' assert { type: 'json' };
+import del from 'rollup-plugin-delete';
+import typescript from 'rollup-plugin-typescript2';
 import { RollupOptions } from 'rollup';
+import mv from 'rollup-plugin-mv';
 
 const bundle = (config: RollupOptions) => ({
   ...config,
@@ -34,11 +38,23 @@ export default [
     plugins: [
       commonjs(),
       nodeResolve(),
-      esbuild({
-        minify: true,
+      del({ targets: ['./dist/*'] }),
+      typescript({
+        tsconfig: 'tsconfig.json',
+        useTsconfigDeclarationDir: true,
       }),
+      terser(),
+      mv([{ src: './dist/src', dest: './dist/types' }], {
+        overwrite: true,
+        once: true,
+      }),
+      del({ targets: './dist/src' }),
+      // esbuild({
+      //   minify: true,
+      // }),
     ],
   }),
+
   // bundle({
   //   plugins: [dts()],
   //   output: {
